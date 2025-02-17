e72a5c3a5ff73a82506640e2497f0105a8b21b28
MandeepBasnet <mandeep3basnet@gmail.com>
2025-02-17 22:21:02 +0545

    Eleventh Commit

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -4,18 +4,18 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         {/* Left Side */}
         <div>
-          <img src={assets.logo} alt="" />
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum illo aliquam ex et quasi itaque sequi eum suscipit officiis a, quod voluptatum nihil. Ducimus atque praesentium architecto incidunt, molestiae unde?</p>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum illo aliquam ex et quasi itaque sequi eum suscipit officiis a, quod voluptatum nihil. Ducimus atque praesentium architecto incidunt, molestiae unde?</p>
         </div>
 
         {/* Center Side */}
         <div>
-          <p>COMPANHY</p>
-          <ul>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
             <li>Home</li>
             <li>About Us</li>
             <li>Contact Us</li>
@@ -25,8 +25,8 @@ const Footer = () => {
 
         {/* Right Side */}
         <div>
-          <p>GET IN TOUCH</p>
-          <ul>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+1 234 567 890</li>
             <li>info@lawyerconnect.com</li>
           </ul>
@@ -36,7 +36,7 @@ const Footer = () => {
         {/* Copyright Text */}
         <div>
           <hr />
-          <p>&copy; 2023 Lawyer Connect. All rights reserved.</p>
+          <p className='py-5 text-sm text-center'>&copy; 2025 Lawyer Connect. All rights reserved.</p>
         </div>
       </div>
     </div>
