f6d09f7fff333a306b8b629877ed3945a54cad3b
MandeepBasnet <mandeep3basnet@gmail.com>
2025-03-22 11:24:00 +0545

    Thirty Commit

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { addlaywer } from "../controllers/adminController";
+import { addlawyer } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 
 const adminRouter = express.Router();
 
-adminRouter.post("/add-lawyer", upload.single("image"), addlaywer);
+adminRouter.post("/add-lawyer", upload.single("image"), addlawyer);
 
 export default adminRouter;
